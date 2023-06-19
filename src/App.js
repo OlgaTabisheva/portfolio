fe311227f6af92823a9355fdb15f3fe81be5196d
Olga Tabisheva <oliatabisheva@gmail.com>
2023-06-19 19:14:39 +0300

    portfolio reformat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Main from "./components/Main/Main";
 import {Link, Route, Routes} from "react-router-dom";
 import Contacts from "./components/Contacts/Contacts";
 import NotFound from "./components/NotFound/NotFound";
@@ -7,7 +6,6 @@ import Projects from "./components/Projects/Projects";
 import Info from "./components/Info/Info";
 import {Menu, MenuItem, Sidebar, useProSidebar} from "react-pro-sidebar";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
-import InfoIcon from "@mui/icons-material/Info";
 import EngineeringIcon from "@mui/icons-material/Engineering";
 import WorkIcon from "@mui/icons-material/Work";
 import ContactsIcon from "@mui/icons-material/Contacts";
@@ -33,10 +31,6 @@ function App() {
               }>
               <h2>Главная</h2>
             </MenuItem>
-            <MenuItem icon={<InfoIcon/>}
-                      component={<Link to="/info" className="link" />}
-            >Информация
-            </MenuItem>
             <MenuItem icon={<EngineeringIcon />}
                       component={<Link to="/skills" className="link" />}
             >
@@ -51,11 +45,10 @@ function App() {
         </Sidebar>
 
       <Routes>
-        <Route path="/info" element={<Info/>}/>
+        <Route path="/" element={<Info/>}/>
           <Route path="/contacts" element={<Contacts/>}/>
           <Route path="/projects" element={<Projects/>}/>
         <Route path="/skills" element={<Skills/>}/>
-          <Route path="/" element={<Main/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
       </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -13,8 +13,12 @@ function Skills() {
           <li className={cls.skills__text}>Redux (Redux Toolkit, React Redux)</li>
           <li className={cls.skills__text}>Axios</li>
           <li className={cls.skills__text}>Библиотеки готовых компонентов (mui, bootstrap)</li>
+          <li className={cls.skills__text}>Система контроля версий Git (GitHub)</li>
+          <li className={cls.skills__text}>Трекер задач (Битрикс24)</li>
           <li className={cls.skills__text}>Docker (развертывание проекта по инструкции) </li>
           <li className={cls.skills__text}>Node.js (только учебный опыт) </li>
+          <li className={cls.skills__text}>Графический редактор figma (верстка по готовому макету)</li>
+          <li className={cls.skills__text}>Канбан доски и пр. (взаимодействие в trello, notion) </li>
         </ol>
     </section>
   )

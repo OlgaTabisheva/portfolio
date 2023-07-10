66cc1e574065de935c1907fd8952239b0645a5c2
Olga Tabisheva <oliatabisheva@gmail.com>
2023-07-10 16:25:02 +0200

    reformat

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -6,8 +6,8 @@ function NotFound() {
   return (
     <section>
       <div className='not-found'>
-        <h2 className='not-found__title'>Ошибка 404</h2>
-        <p className='not-found__text'>К сожалению данная страница не доступна.</p>
+        <h2 className='not-found__title'>{t('404')}</h2>
+        <p className='not-found__text'>{t('404text')}</p>
         <img width="200px"
              src={sad} alt="sad"/>
       </div>

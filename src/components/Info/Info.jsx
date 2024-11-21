80b2401d2dc6e117ee404ae353d66d30515409ca
OlgaTabisheva <oliatabisheva@gmail.com>
2024-11-21 19:56:40 +0100

    update

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,6 +1,6 @@
 import cls from "./Info.module.scss";
 import { useTranslation } from "react-i18next";
-import photo from './../../images/Gemini_Generated_Image_11p5mt11p5mt11p5.jpeg'
+import photo from "./../../images/Gemini_Generated_Image_11p5mt11p5mt11p5.jpeg";
 
 function Info() {
   const { t } = useTranslation();
@@ -9,16 +9,23 @@ function Info() {
       {/* <h2>{t("Hi all")}</h2> */}
       <div className={cls.info__box}>
         <div className={cls.info__boxText}>
-        <p className={cls.info__par}>{t("paragraph 1")}</p>
-        <p className={cls.info__par}>{t("paragraph 2")}</p>
+          <p className={cls.info__par}>{t("paragraph 1")}</p>
+          <ol className={cls.info__listSmall}>
+            <li className={cls.info__text}>{t("paragraph 2")}</li>
+            <li className={cls.info__text}>{t("paragraph 2.1")}</li>
+            <li className={cls.info__text}>{t("paragraph 2.2")}</li>
+            <li className={cls.info__text}>{t("paragraph 2.3")}</li>
+            <li className={cls.info__text}>{t("paragraph 2.4")}</li>
+          </ol>
         </div>
-        <img className={cls.info__img} alt='cat' src={photo}/>
-        </div>
-        <p className={cls.info__par}>{t("paragraph 3")}</p>
-        <p className={cls.info__par}>{t("paragraph 4")}</p>
-        <p className={cls.info__par}>{t("paragraph 4.1")}</p>
-{/*        // <p className={cls.info__par}>{t("paragraph 4.2")}</p>
- */}    </section>
+        <img className={cls.info__img} alt="cat" src={photo} />
+      </div>
+      <p className={cls.info__par}>{t("paragraph 3")}</p>
+      <p className={cls.info__par}>{t("paragraph 4")}</p>
+      <p className={cls.info__par}>{t("paragraph 4.1")}</p>
+      {/*        // <p className={cls.info__par}>{t("paragraph 4.2")}</p>
+       */}{" "}
+    </section>
   );
 }
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import cls from "./Project.module.scss";
 import { useTranslation } from "react-i18next";
 import imgCook from "./../../images/Screenshot1.png";
+import imgKnot from "./../../images/Screenshot_20241121_161847.png";
 function Projects() {
   const { t } = useTranslation();
   return (
@@ -11,39 +12,88 @@ function Projects() {
             <div className={cls.project__textBox}>
               <p className={cls.project__text}>{t("project1")}</p>
               <p className={cls.project__text}>{t("project1.1")}</p>
+              <p className={cls.project__text}>{t("project1.2")}</p>
+              <p className={cls.project__text}>{t("project1.3")}</p>
+              <ol className={cls.project__listSmall}>
+                <li className={cls.project__text}>{t("project1.4")}</li>
+                <li className={cls.project__text}>{t("project1.5")}</li>
+                <li className={cls.project__text}>{t("project1.6")}</li>
+                <li className={cls.project__text}>{t("project1.7")}</li>
+                <li className={cls.project__text}>{t("project1.8")}</li>
+              </ol>
+              <p className={cls.project__text}>{t("project1.9")}</p>
+              <ol className={cls.project__listSmall}>
+                <li className={cls.project__text}>{t("project1.10")}</li>
+                <li className={cls.project__text}>{t("project1.11")}</li>
+              </ol>
+              <p className={cls.project__text}>{t("project1.12")}</p>
             </div>
             <a
               className={cls.project__linkCover}
               href="https://cook-book.746746746.xyz/recipes"
             >
               <img className={cls.project__img} src={imgCook} alt="cookBook" />
-              <p className={cls.project__linkText}>Проект Cook-book.</p>
-              <p className={cls.project__linkTextSmall}>
-                Готовка станет твоим новым хобби. С нашими пошаговыми рецептами
-                и полезными советами даже начинающий кулинар сможет приготовить
-                настоящее произведение искусства.
-              </p>
-            </a>
-          </li>
-          <li>
-            <a
-              className={cls.project__link}
-              href="https://knot-store.746746746.xyz/"
-            >
-              <div>
-                <p className={cls.project__text}>{t("project3")}</p>
-                <p className={cls.project__text}>{t("project3.1")}</p>
+              <div className={cls.project__textBox}>
+                <p className={cls.project__linkText}>
+                  Cсылка на проект: 'Cook-book'.
+                </p>
+                <p className={cls.project__linkTextSmall}>
+                  Готовка станет твоим новым хобби. С нашими пошаговыми
+                  рецептами и полезными советами даже начинающий кулинар сможет
+                  приготовить настоящее произведение искусства.
+                </p>
               </div>
             </a>
           </li>
-          <li>
+          <div className={cls.project__createLine}></div>
+          <li className={cls.project__listBox}>
+            <a
+              className={cls.project__linkCover}
+              href="https://knot-store.746746746.xyz/"
+            >
+              <img className={cls.project__img} src={imgKnot} alt="knotStore" />
+              <div className={cls.project__textBox}>
+                <p className={cls.project__linkText}>
+                  Cсылка на проект: 'Knot-store'.
+                </p>
+                <p className={cls.project__linkTextSmall}>
+                  Здесь, в этом маленьком магазине, каждая вещь – уникальное
+                  произведение искусства, созданное вручную с любовью и
+                  вниманием к деталям.
+                </p>
+              </div>
+            </a>
+            <div className={cls.project__textBox}>
+            <div className={cls.project__textBox}>
+              <p className={cls.project__text}>{t("project3")}</p>
+              <p className={cls.project__text}>{t("project3.1")}</p>
+              <p className={cls.project__text}>{t("project3.2")}</p>
+              <p className={cls.project__text}>{t("project3.3")}</p>
+              <ol className={cls.project__listSmall}>
+                <li className={cls.project__text}>{t("project3.4")}</li>
+                <li className={cls.project__text}>{t("project3.5")}</li>
+                <li className={cls.project__text}>{t("project3.6")}</li>
+                <li className={cls.project__text}>{t("project3.7")}</li>
+                <li className={cls.project__text}>{t("project3.8")}</li>
+                <li className={cls.project__text}>{t("project3.9")}</li>
+                <li className={cls.project__text}>{t("project3.10")}</li>
+              </ol>
+              <p className={cls.project__text}>{t("project3.11")}</p>
+              <ol className={cls.project__listSmall}>
+                <li className={cls.project__text}>{t("project3.12")}</li>
+              </ol>
+              <p className={cls.project__text}>{t("project1.12")}</p>
+            </div>
+            </div>
+          </li>
+          {/*         <li>
             <a
               className={cls.project__link}
               href="https://demoshop.746746746.xyz/"
             >
               <p className={cls.project__text}>{t("project2")}</p>
             </a>
-          </li>
+          </li> */}
         </ol>
       </div>
       {/*      <div className={cls.project__block}>

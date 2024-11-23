30a1e1fa0595064011f66f288481c94968b0e7a7
OlgaTabisheva <oliatabisheva@gmail.com>
2024-11-23 17:18:26 +0100

    update

--- src/App.js
+++ src/App.js
@@ -11,18 +11,54 @@ import cls from "./components/Skills/Skills.module.scss";
 import home from "./images/home.svg";
 import skills from "./images/computer.svg";
 import education from "./images/skills_5gavycb1qcik.svg";
+import flagRu from "./images/russia_144dij0xz4cy.svg";
+import flagEn from "./images/united_states_6wn5hj71yyi5.svg";
 
 import work from "./images/work.svg";
 import cnt from "./images/contact.svg";
 import { useTranslation } from "react-i18next";
 import Education from "./components/Education/Education";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   const { t, i18n } = useTranslation();
+  const getUserLanguage =
+    window.navigator.userLanguage || window.navigator.language;
+  const [useLanguage, setUseLanguage] = useState(getUserLanguage);
+
+  function changeUserLanguage() {
+    if (useLanguage === "en") {
+      setUseLanguage("ru"); 
+       i18n.changeLanguage("ru");
+    }
+    else 
+    if (useLanguage === "ru") {
+      setUseLanguage("en");
+        i18n.changeLanguage("en");
+    }
+  }
+
+  useEffect(() => {
+    i18n.changeLanguage(getUserLanguage);
+  }, []);
 
   return (
     <div className={style.app}>
+         
       <div id="outer-container" className={style.app__outerContainer}>
+      <div className={style.app__buttonFlagsCover}>
+            <button
+              className={style.app__buttonFlags}
+              onClick={() => changeUserLanguage()}
+            >
+              <img
+                className={style.app__imgFlags}
+                alt="flag"
+                src={useLanguage === "ru" ? flagRu : flagEn}
+              />
+            </button>
+          </div>
         <div className={style.app__menuBox}>
           <Menu
             customBurgerIcon={
@@ -42,18 +78,6 @@ function App() {
                 {t("Main")}
               </div>
             </a>
-
-            <a className={style.app__menuItem} href="#skills">
-              <div>
-                <img
-                  className={cls.skills__img}
-                  src={skills}
-                  id="skills2"
-                  alt="skills1"
-                />
-                {t("Skills")}
-              </div>
-            </a>
             <a className={style.app__menuItem} href="#projects">
               <div className={cls.skills__box}>
                 <img
@@ -65,6 +89,18 @@ function App() {
                 {t("Projects")}
               </div>
             </a>
+            <a className={style.app__menuItem} href="#skills">
+              <div className={cls.skills__box}>
+                <img
+                  className={cls.skills__img}
+                  src={skills}
+                  id="skills2"
+                  alt="skills1"
+                />
+                {t("Skills")}
+              </div>
+            </a>
+
             <a className={style.app__menuItem} href="#education">
               <div className={cls.skills__box}>
                 <img
@@ -83,9 +119,10 @@ function App() {
               </div>
             </a>
           </Menu>
+     
         </div>
         <nav className={style.app__nav}>
-          <select
+          {/*        <select
             className={style.app__menuButtons}
             onChange={(e) => i18n.changeLanguage(e.target.value)}
           >
@@ -96,21 +133,14 @@ function App() {
             <option className={style.app__menuButton} value="en">
               Choose language/English
             </option>
-          </select>
+          </select> */}
 
           <div className={style.app__menuItemBox}>
             <a className={style.app__menuItemLink} href="#info" id="info">
-              Информация
+              {t("Hi all")}
             </a>
             <Info />
           </div>
-          <div className={style.app__menuItemBox}>
-            <a className={style.app__menuItemLink} href="#skills" id="skills">
-              {t("Skills")}
-            </a>
-            <Skills />
-          </div>
-
           <div className={style.app__menuItemBox}>
             <a
               className={style.app__menuItemLink}
@@ -121,6 +151,13 @@ function App() {
             </a>
             <Projects />
           </div>
+          <div className={style.app__menuItemBox}>
+            <a className={style.app__menuItemLink} href="#skills" id="skills">
+              {t("Skills")}
+            </a>
+            <Skills />
+          </div>
+
           <div className={style.app__menuItemBox}>
             <a
               className={style.app__menuItemLink}
